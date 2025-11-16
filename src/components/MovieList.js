5d53f09a74b8429f7a4f5fea50f9eb79564472cb
Prasoon Kumar <prasoon9582@gmail.com>
2025-11-16 18:36:40 +0530

    added custom-scroll

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,7 +5,7 @@ const MovieList = ({ title, movies }) => {
   return (
     <div className="px-6">
       <h1 className="text-3xl font-bold py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-scroll no-scrollbar">
+      <div className="flex overflow-x-scroll custom-scrollbar">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard key={movie.id} posterPath={movie.poster_path} />
