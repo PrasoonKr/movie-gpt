5b231268841225eb3e26dc72be5c681d056c405c
Prasoon Kumar <prasoon9582@gmail.com>
2025-11-17 02:25:54 +0530

    bug fix

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -5,20 +5,26 @@ import { addTrailerVideo } from "../utils/movieSlice";
 
 const useMovieTrailer = (movieId) => {
   const dispatch = useDispatch();
+
   const getmovieVideos = async () => {
     const data = await fetch(
-      "https://api.themoviedb.org/3/movie/" +
-        movieId +
-        "/videos?language=en-US",
+      "https://api.themoviedb.org/3/movie/" + movieId + "/videos?language=en-US",
       API_OPTIONS
     );
     const json = await data.json();
-    const filterData = json.results.filter((video) => video.name === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideo(trailer));
+
+    const results = json?.results || [];
+
+    const filterData = results.filter((video) => video.name === "Trailer");
+    const trailer = filterData.length ? filterData[0] : results[0];
+
+    if (trailer) dispatch(addTrailerVideo(trailer));
   };
+
   useEffect(() => {
+    if (!movieId) return;        //  prevents undefined fetch
     getmovieVideos();
-  }, []);
+  }, [movieId]);                 // depend on movieId
 };
+
 export default useMovieTrailer;
